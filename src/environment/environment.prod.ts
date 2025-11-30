969901da9ceba619002aed3fbced4446191fc199
skaescobedo <luuisescoobedo@gmail.com>
2025-11-30 00:21:01 -0700

    papa

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl: 'https://easyquizzyapi.up.railway.app',
   wsUrl: 'wss://easyquizzyapi.up.railway.app',
   // aquí va el dominio donde publiques el Angular (por ejemplo en Vercel, Netlify, etc.)
-  domainUrl: 'https://TU-DOMINIO-FRONTEND.com',
+  domainUrl: 'https://easyquizzy-production.up.railway.app',
   googleClientId: '766266417315-vracakdph9p31q09is106lrk0k19plcp.apps.googleusercontent.com'
 };
